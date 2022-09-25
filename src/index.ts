8fe151c773db124dab47941e59ff0263c1dbfc7b
benNjogu <kamandemachafi@gmail.com>
2022-09-25 21:30:08 +0300

    enum part 1

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-let user: [number, string] = [1, "Mosh"];
+const small = 1;
+const medium = 2;
+const large = 3;
+
+enum Size {
+  Small = 1,
+  Medium,
+  Large,
+}
+
+let mySize: Size = Size.Medium;
+console.log(mySize);
