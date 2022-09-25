8756a4474d82179fdb3632d462fb362777209109
benNjogu <kamandemachafi@gmail.com>
2022-09-25 21:55:13 +0300

    Functions with a default value

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-function calculateTax(income: number, taxYear?: number): number {
-  if ((taxYear || 2022) < 2022) return income * 1.2;
+function calculateTax(income: number, taxYear = 2022): number {
+  if (taxYear < 2022) return income * 1.2;
 
   return income * 1.3;
 }
