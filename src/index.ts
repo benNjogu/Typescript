70330994fb2d647b1238df250c69f2fe447689d5
benNjogu <kamandemachafi@gmail.com>
2022-09-25 17:30:24 +0300

    array part 2

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
-let numbers: number[] = [1, 2, "3"];
-//The 3 causes an error
+//let numbers: number[] = [1, 2, "3"];
+
+let numbers: number[] = [];
+numbers[0] = 1;
+numbers[1] = "a";
+//The as causes an error
