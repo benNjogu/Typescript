c26ad99897c390a32797d0ea67817c572f993216
benNjogu <kamandemachafi@gmail.com>
2022-09-25 17:29:15 +0300

    arrays part 1

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,2 @@
-let sales = 123_456_789;
-let course = "Typescript";
-let isPublished = true;
-let level;
-
-function render(document: any) {
-  console.log(document);
-}
+let numbers: number[] = [1, 2, "3"];
+//The 3 causes an error
