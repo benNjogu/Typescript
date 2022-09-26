37016688740f23477be05daaa249419f9bcaeb0a
benNjogu <kamandemachafi@gmail.com>
2022-09-26 15:37:00 +0300

    The unknown type

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
-let phone = <HTMLInputElement>document.getElementById("phone");
-//HTMLElement
-//HTMLInputElement
-phone.value;
+function render(document: unknown) {
+  //Narrowing
+  if (typeof document === "string") document.toUpperCase();
+  //typeof operator works for primitive types
+
+  if (document instanceof wordDocument) {
+    //use instance of for object types
+  }
+  document.move();
+}
