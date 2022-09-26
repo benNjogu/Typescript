35448d8b91a3b033ba4c11a1b1b93c65903cb0f8
benNjogu <kamandemachafi@gmail.com>
2022-09-26 15:45:38 +0300

    The never type

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
-function render(document: unknown) {
-  //Narrowing
-  if (typeof document === "string") document.toUpperCase();
-  //typeof operator works for primitive types
-
-  if (document instanceof wordDocument) {
-    //use instance of for object types
-  }
-  document.move();
+function reject(message: string): never {
+  throw new Error(message);
 }
+
+function processEvents(): never {
+  //to tell the compiler that this fn never returns
+  while (true) {
+    //Read a message from a queue
+  }
+}
+
+reject("...");
+console.log("Hello world");
