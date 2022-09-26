1b436f25df8ab8c9e3c3da07cce8cdbc30f88be8
benNjogu <kamandemachafi@gmail.com>
2022-09-26 15:25:42 +0300

    Type assertions using as keyword

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
-let speed: number | null = null;
-let ride = {
-  //Falsy (undefined, null, '', false, 0)
-  //The two below statements mean the same thing
-  //speed: speed !== null ? speed : 30,
-  speed: speed ?? 30, //Nullish coalescing operator
-};
+let phone = document.getElementById("phone") as HTMLInputElement;
+//HTMLElement
+//HTMLInputElement
+phone.value;
