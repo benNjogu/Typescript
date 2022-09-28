3767380655096c9e982c625fdee8d2ea5b2d6d7a
benNjogu <kamandemachafi@gmail.com>
2022-09-28 23:14:57 +0300

    Abstract classes and methods

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,18 @@
-class Person {
-  constructor(public firstName: string, public lastName: string) {}
+abstract class Shape {
+  constructor(public color: string) {}
 
-  get fullName() {
-    return this.firstName + " " + this.lastName;
+  abstract render(): void;
+}
+
+class Circle extends Shape {
+  constructor(public radius: number, color: string) {
+    super(color);
   }
 
-  //with protected keyword, walk can be inherited.
-  protected walk() {
-    console.log("walking");
+  override render(): void {
+    console.log("Rendering a circle");
   }
 }
 
-class Student extends Person {
-  constructor(public studentId: number, firstName: string, lastName: string) {
-    super(firstName, lastName);
-  }
-
-  takeTest() {
-    this.walk();
-    console.log("Taking a test");
-  }
-}
-
-class Principal extends Person {
-  override get fullName() {
-    return "Principal " + super.fullName;
-  }
-}
-
-class Teacher extends Person {
-  override get fullName() {
-    return "Prof. " + super.fullName;
-  }
-}
-
-printNames([
-  new Student(1, "John", "Smith"),
-  new Principal("Nderitu", "Gachagua"),
-  new Teacher("Tabut", "Choge"),
-]);
-
-function printNames(people: Person[]) {
-  for (let person of people) console.log(person.fullName);
-}
-
+let shape = new Circle(13, "red");
+shape.render();
