68c75a1704b4aa265d562effe9d4c918d5382fc1
benNjogu <kamandemachafi@gmail.com>
2022-09-28 08:09:25 +0300

    Parameter properties

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,11 @@
 class Account {
-  readonly id: number;
-  owner: string;
-  private _balance: number;
   nickName?: string;
 
-  constructor(id: number, owner: string, balance: number) {
-    this.id = id;
-    this.owner = owner;
-    this._balance = balance;
-  }
+  constructor(
+    public readonly id: number,
+    public owner: string,
+    private _balance: number
+  ) {}
 
   deposit(amount: number): void {
     if (amount <= 0) throw new Error("Invalid amount");
@@ -26,6 +23,3 @@ class Account {
 let account = new Account(1, "Ben", 980);
 account.deposit(1000);
 console.log(account.getBalance);
-
-
-
