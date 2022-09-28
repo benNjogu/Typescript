4f4f07d676a96efcf742e7594dd3ebe429781cbd
benNjogu <kamandemachafi@gmail.com>
2022-09-28 08:45:35 +0300

    Index signatures.

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,8 @@
-class Account {
-  nickName?: string;
-
-  constructor(
-    public readonly id: number,
-    public owner: string,
-    private _balance: number
-  ) {}
-
-  deposit(amount: number): void {
-    if (amount <= 0) throw new Error("Invalid amount");
-
-    this._balance += amount;
-  }
-
-  private calculateTax() {}
-
-  get balance(): number {
-    return this._balance;
-  }
-
-  set balance(value: number) {
-    if (value < 0) throw new Error("Invalid value");
-    this._balance = value;
-  }
+class SeatAssignment {
+  //Index signature property
+  [seatNumber: string]: string;
 }
 
-let account = new Account(1, "Ben", 980);
-account.balance = 250;
-account.deposit(1000);
-console.log(account.balance);
+let seats = new SeatAssignment();
+seats.A1 = "Ben";
+seats.A2 = "Lenox";
