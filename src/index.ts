9678ee1d18cd3a1d0e2d0352da3ac2e667769b25
benNjogu <kamandemachafi@gmail.com>
2022-09-29 15:07:56 +0300

    Constraining generics part 1

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,5 @@
-interface Result<T> {
-  data: T | null;
-  error: string | null;
+function echo<T extends number | string>(value: T): T {
+  return value;
 }
 
-function fetch<T>(url: string): Result<T> {
-  return { data: null, error: null };
-}
-
-interface User {
-  username: string;
-}
-
-interface Product {
-  title: string;
-}
-
-let result = fetch<User>("url");
-result.data?.username;
+echo("true");
