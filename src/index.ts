99e7489e764abb2a26fec0b0b72e037dd93c7838
benNjogu <kamandemachafi@gmail.com>
2022-09-29 16:25:51 +0300

    Type mapping

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,21 @@ interface Product {
   price: number;
 }
 
-class Store<T> {
-  protected _objects: T[] = [];
+type ReadOnly<T> = {
+  readonly //index signature
+  //keyof operator
+  [K in keyof T]: T[K];
+};
 
-  add(obj: T): void {
-    this._objects.push(obj);
-  }
+type Optional<T> = {
+  [K in keyof T]?: T[K];
+};
 
-  find(property: keyof T, value: unknown): T | undefined {
-    return this._objects.find((obj) => obj[property] === value);
-  }
-}
+type Nullable<T> = {
+  [K in keyof T]?: T[K] | null;
+};
 
-let store = new Store<Product>();
-store.add({ name: "a", price: 1 });
-store.find("name", "a");
-store.find("price", 1);
-store.find("nonExistingProperty", 1);
+let product: ReadOnly<Product> = {
+  name: "a",
+  price: 1,
+};
