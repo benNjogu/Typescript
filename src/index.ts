ad13152e86a26383f95346a3f5abfc9acaf7033d
benNjogu <kamandemachafi@gmail.com>
2022-09-29 16:09:58 +0300

    The keyof operator

--- src/index.ts
+++ src/index.ts
@@ -9,29 +9,14 @@ class Store<T> {
   add(obj: T): void {
     this._objects.push(obj);
   }
-}
-//Pass on the generic type parameter
-class CompressibleStore<T> extends Store<T> {
-  compress() {}
-}
 
-class Apple implements Product {
-  constructor(public name: string, public price: number) {}
-}
-
-let store = new CompressibleStore<Product>();
-store.add(new Apple("Apple", 30));
-
-//Restricting the generic type parameter.
-class SearchableStore<T extends { name: string }> extends Store<T> {
-  find(name: string): T | undefined {
-    return this._objects.find((obj) => obj.name === name);
+  find(property: keyof T, value: unknown): T | undefined {
+    return this._objects.find((obj) => obj[property] === value);
   }
 }
 
-//Fix the generic type parameter
-class ProductStore extends Store<Product> {
-  filterByCategory(category: string): Product[] {
-    return [];
-  }
-}
+let store = new Store<Product>();
+store.add({ name: "a", price: 1 });
+store.find("name", "a");
+store.find("price", 1);
+store.find("nonExistingProperty", 1);
