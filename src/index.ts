384b76cee9655bf6078f5fc3f48d0aa56289e33d
benNjogu <kamandemachafi@gmail.com>
2022-09-30 10:06:14 +0300

    Parameterized decorators

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
-function Component(constructor: Function) {
-  console.log("Component decorator called");
-  constructor.prototype.uniqueId = Date.now();
-  constructor.prototype.insertInDom = () => {
-    console.log("Inserting the component in the DOM");
+type ComponentOptions = {
+  selector: string;
+};
+
+function Component(options: ComponentOptions) {
+  return (constructor: Function) => {
+    console.log("Component decorator called");
+    constructor.prototype.options = options;
+    constructor.prototype.uniqueId = Date.now();
+    constructor.prototype.insertInDom = () => {
+      console.log("Inserting the component in the DOM");
+    };
   };
 }
 
-@Component
+@Component({ selector: "#my-profile" })
 class ProfileComponent {}
